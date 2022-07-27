3789bdbefd21e773fe4b60ace00bbac5bfd4b06a
Asutton <asutton1998@yahoo.com>
2022-07-27 13:08:56 -0400

    Add Flask Session

--- app.py
+++ app.py
@@ -10,18 +10,24 @@ debug = DebugToolbarExtension(app)
 
 
 responses=[]
+
 @app.route('/')
 def show_start_survey():
     return render_template('survey.html', survey = survey)
 
 @app.route('/begin', methods=['POST'])
 def start_survey():
+    session['responses'] = []
     return redirect('/questions/0')
 
 @app.route('/answer',methods=['POST'])
 def handle_question():
     choice = request.form['answer']
+
+    responses = session['responses'] 
     responses.append(choice)
+    session['responses'] = responses
+
     if (len(responses) == len(survey.questions)):
         return redirect('/complete')
     else:
@@ -29,6 +35,10 @@ def handle_question():
 
 @app.route('/questions/<int:id>')
 def show_question(id):
+    responses = session.get('responses')
+    if (responses is None):
+        return redirect('/')
+
     question = survey.questions[id]
     if(len(responses) != id):
         flash(f'Invalid Question ID:{id} ','error')
